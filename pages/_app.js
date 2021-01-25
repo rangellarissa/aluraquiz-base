ee0145b18ccac44a1d355cf200eedbedc93865bc
rangellarissa <larissarangel.oc@gmail.com>
2021-01-25 20:42:15 -0300

    Código final da primeira aula

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,39 @@
-import App from 'next/app'
-import { ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
+const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    margin: 0;
+    padding: 0;
+    /*New Styles*/
+    display:flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`
+const theme = db.theme;
 
-    return (
+export default function App ({ Component, pageProps}) {
+  return (
+    <>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    )
-  }
+    </>
+  )
 }
